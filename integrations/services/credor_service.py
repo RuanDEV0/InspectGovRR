c7a24a4fc91005daea6aebda078a5499d1f8f8f0
Ruan <oliveiraruan2018@gmail.com>
2026-04-14 21:39:28 -0400

    fix: add atribute c.name in group by

--- integrations/services/credor_service.py
+++ integrations/services/credor_service.py
@@ -4,7 +4,7 @@ import asyncio
 def get_list_credors_by_total_pago():
     with connection.cursor() as cursor:
         cursor.execute("select c.name, c.cnpj, c.total_liquidado, sum(c.total_pago), c.quantidade_pagamentos"
-                       " from integrations_credor as c group by c.name, c.cnpj, c.total_liquidado, c.quantidade_pagamentos "
+                       " from integrations_credor as c group by c.name, c.cnpj, c.total_liquidado, c.quantidade_pagamentos, c.total_pago "
                        "order by c.total_pago desc limit 10")
 
         return cursor.fetchall()
