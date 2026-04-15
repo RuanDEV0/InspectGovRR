599c4b821dc5576eb83fc307ed9825353a784978
Ruan <oliveiraruan2018@gmail.com>
2026-04-14 21:42:06 -0400

    feat: implemented layer http new resources

--- integrations/urls.py
+++ integrations/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('get-token', views.get_token, name='get_token'),
     path("get-units", views.get_units, name='get_units'),
     path("get-dailies", views.get_dailies, name='get_dailies'),
+    path("get-exec-orcament", views.get_exec_orcament, name='get_exec_orcament'),
+    path("get-list-credor", views.get_list_credors_by_total_pago, name='get_list_credors_by_total_pago'),
 ]
